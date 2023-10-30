aaaa6d9421e93836b1f69a2d8b219090d2f8347f
TiagoSabath <tiago.sabath@gmail.com>
2023-10-30 08:55:56 -0300

    Aula2

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -8,9 +8,13 @@ namespace ExemplosFundamentos.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int Idade { get; set; }
-        
+         public Pessoa()
+        {
+            this.Nome = Nome;
+            this.Idade = Idade;
+        }
         public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
--- Program.cs
+++ Program.cs
@@ -5,3 +5,92 @@ Pessoa p = new Pessoa();
 p.Nome = "Tiago";
 p.Idade = 25;
 p.Apresentar();
+
+
+//Condição em if e else
+
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a Venda: {possivelVenda}");
+
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida.");
+}
+else if (possivelVenda)
+{
+    Console.WriteLine("Venda Realizada.");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}
+
+
+// Condição com switch
+
+Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
+
+
+// Operador OR
+
+bool ehMaiordeIdade = true;
+bool possuiAutorizacaoDoRespossavel = false;
+
+if (ehMaiordeIdade || possuiAutorizacaoDoRespossavel)
+{
+    Console.WriteLine("Entrada Liberada!");
+}
+else{
+    Console.WriteLine("Entrada não liberada!");
+}
+
+
+// Operador AND
+
+bool possuiPresencaMinima = true;
+double media = 7.5;
+
+if (possuiPresencaMinima && media >= 7)
+{
+    Console.WriteLine("Aprovado!");
+}
+else
+{
+    Console.WriteLine("Reprovado!");
+}
+
+
+// Operador NOT
+
+bool choveu = true;
+bool estaTarde = true;
+
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar");
+}
+else
+{
+    Console.WriteLine("Vou pedalar um outro dia");
+}
